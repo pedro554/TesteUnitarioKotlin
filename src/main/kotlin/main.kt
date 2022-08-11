645a1926550480220174c84b4ea6aeda5df11c83
Pedro Viero <pedrohenriqueviero@outlook.com>
2022-08-11 17:01:24 -0300

    ex

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -23,6 +23,28 @@ fun excecao(): Boolean {
 //    throw NullPointerException()
 }
 
+fun portaria(idade: Int, tpConvite: String, cod: String): String {
+    if (idade < 18) return "Negado."
+
+    if (tpConvite != "") {
+        val tipoConvite = tpConvite.lowercase()
+
+        if (tipoConvite != "comun" && tipoConvite != "premium" && tipoConvite != "luxo") return "Negado."
+
+        if (cod != "") {
+            val codigo = cod.lowercase()
+            return if (tipoConvite == "comun" && codigo.startsWith("xt")) {
+                "Welcome."
+            } else if ((tipoConvite == "premium" || tipoConvite == "luxo") && codigo.startsWith("xl")) {
+                "Welcome."
+            } else {
+                "Negado."
+            }
+        }
+    }
+    return "Negado."
+}
+
 fun main() {
     println(countXO("xxoo"))
     println(countXO("xxooo"))
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,35 +1,49 @@
 import org.junit.jupiter.api.*
 
 class MainTest {
-    @Test
-    @DisplayName("Teste metodo xxoo")
-    fun testCountXO() {
-        Assertions.assertAll(
-            {Assertions.assertTrue(countXO("xxoo"))},
-            { Assertions.assertFalse(countXO("xxooo")) },
-            { Assertions.assertEquals(countXO("xxoo"), true) },
-            { Assertions.assertTrue(countXO("xXoo")) },
-            { Assertions.assertTrue(countXO("XXOO")) },
-        )
-    }
+//    @Test
+//    @DisplayName("Teste metodo xxoo")
+//    fun testCountXO() {
+//        Assertions.assertAll(
+//            {Assertions.assertTrue(countXO("xxoo"))},
+//            { Assertions.assertFalse(countXO("xxooo")) },
+//            { Assertions.assertEquals(countXO("xxoo"), true) },
+//            { Assertions.assertTrue(countXO("xXoo")) },
+//            { Assertions.assertTrue(countXO("XXOO")) },
+//        )
+//    }
+//
+//    @Test
+//    @Disabled
+//    fun naoImplementado() { }
+//
+//    @Test
+//    fun vaiFalhar() {
+//        fail("Não pode terminar o teste sem este metodo")
+//    }
+//
+//    @Test
+//    fun assumption() {
+//        Assumptions.assumeTrue(countXO("xxoo"))
+//        Assertions.assertTrue(abc())
+//    }
+//
+//    @Test
+//    fun exception() {
+//        assertThrows<NullPointerException> { excecao() }
+//    }
 
     @Test
-    @Disabled
-    fun naoImplementado() { }
-
-    @Test
-    fun vaiFalhar() {
-        fail("Não pode terminar o teste sem este metodo")
-    }
-
-    @Test
-    fun assumption() {
-        Assumptions.assumeTrue(countXO("xxoo"))
-        Assertions.assertTrue(abc())
-    }
-
-    @Test
-    fun exception() {
-        assertThrows<NullPointerException> { excecao() }
+    @DisplayName("Testa os cenarios da portaria")
+    fun testPortaria() {
+        Assertions.assertEquals(portaria(0, "", ""), "Negado.")
+        Assertions.assertEquals(portaria(20, "", ""), "Negado.")
+        Assertions.assertEquals(portaria(24, "abc", ""), "Negado.")
+        Assertions.assertEquals(portaria(24, "comun", "abc"), "Negado.")
+        Assertions.assertEquals(portaria(24, "comun", "xt"), "Welcome.")
+        Assertions.assertEquals(portaria(24, "premium", "abc"), "Negado.")
+        Assertions.assertEquals(portaria(24, "premium", "xl"), "Welcome.")
+        Assertions.assertEquals(portaria(24, "luxo", "abc"), "Negado.")
+        Assertions.assertEquals(portaria(24, "luxo", "xl"), "Welcome.")
     }
 }
